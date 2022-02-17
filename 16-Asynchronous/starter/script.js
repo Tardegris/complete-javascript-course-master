fecda5d871331318f1e977376fd3da0530b4bbf5
Tardegris <barzaxv@outlook.com>
2022-02-17 10:49:15 -0600

    b

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,6 +1,18 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const oneword = function (str) {
+    return str.replace(/ /g, '').toLowerCase;
+}
 
-///////////////////////////////////////
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+// Higher -order function
+const transformer = function(str, fn) {
+    console.log(str);
+console.log(`transformed string ${fn(str)}`);
+}
+
+transformer('JavaScript is the best!', upperFirstWord);
